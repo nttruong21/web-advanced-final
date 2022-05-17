c3d6349aad7872750d4f9b9cf101567b2c66f498
frgryr2001 <nhanle.lx@gmail.com>
2022-05-17 11:17:14 +0700

    Hoàn thành gửi gmail

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,5 +1,8 @@
 module.exports = (fn) => (req, res, next) => {
   fn(req, res, next).catch((err) => {
-    console.error(err);
+    res.status(500).json({
+      status: 'fail',
+      message: err.message,
+    });
   });
 };
