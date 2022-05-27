dcb715cbc8564ca37ce3a2acbeb34f75f840c3a0
Marsulami <traidat8sao@gmail.com>
2022-05-27 22:53:27 +0700

    update

--- src/app/models/Transaction.js
+++ src/app/models/Transaction.js
@@ -5,15 +5,15 @@ const transactionSchema = new Schema(
     {
         transactionType: { type: Number },
         price: { type: Number },
-        status: { type: Number },
+        status: { type: Number },// 0 nạp tiền, 1 rút tiền, 2 chuyển tiền
         senderPhone: { type: String },
         senderName: { type: String },
         receiverPhone: { type: String },
         message: { type: String },
-        phoneCardCode: { type: String },
-        phoneCardQuantity: { type: Number },
+        phoneCardCode: { type: String },// Mã thẻ cào (viettel, vinaphone, mobifone)
+        phoneCardQuantity: { type: Number },// Số lượng thẻ cào
         transactionFee: { type: String },
-        isFeeForSender: { type: Number },
+        isFeeForSender: { type: Number },// 0 người gửi, 1 người nhận
     },
     {
         timestamps: true,
