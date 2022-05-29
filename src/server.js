7a5f46afc87608b09f790f9e9e88777c14019ee1
nttruong21 <nguyenthetruong100621@gmail.com>
2022-05-29 20:55:58 +0700

    chức năng duyệt giao dịch rút tiền + chuyển tiền trong admin

--- src/server.js
+++ src/server.js
@@ -26,14 +26,25 @@ app.engine(
         extname: "hbs",
         helpers: {
             getDateFormat: function (date) {
-                return new Date(date).toLocaleDateString();
+                return new Date(date).toLocaleDateString("vi-VN");
             },
+            // KIỂM TRA CÓ PHẢI TÀI KHOẢN ĐANG CHỜ KÍCH HOẠT
             isWaitingActiveAccount: function (status) {
                 return status === 0;
             },
+            // KIỂM TRA CÓ PHẢI TÀI KHOẢN ĐANG BỊ KHÓA
             isLockingAccount: function (status) {
                 return status === 5;
             },
+            // KIỂM TRA CÓ PHẢI GIAO DỊCH ĐANG CHỜ DUYỆT
+            isWaitingApproveTransaction: function (status) {
+                return status === 0;
+            },
+            // KIỂM TRA CÓ PHẢI GIAO DỊCH RÚT TIỀN
+            isWithdrawalTransaction: function (type) {
+                return type === 1;
+            },
+            // RENDER TRẠNG THÁI TÀI KHOẢN
             renderAccountStatus: function (status) {
                 switch (status) {
                     case 0:
@@ -44,6 +55,17 @@ app.engine(
                         return `<p class="text-danger font-weight-bold mb-0">Đã bị vô hiệu hóa</p>`;
                     case 5:
                         return `<p class="text-warning font-weight-bold mb-0">Đang bị khóa</p>`;
+                }
+            },
+            // RENDER TRẠNG THÁI GIAO DỊCH
+            renderTransactionStatus: function (status) {
+                switch (status) {
+                    case 0:
+                        return `<p class="text-primary font-weight-bold mb-0">Đang chờ duyệt</p>`;
+                    case 1:
+                        return `<p class="text-success font-weight-bold mb-0">Đã duyệt</p>`;
+                    case 2:
+                        return `<p class="text-danger font-weight-bold mb-0">Đã hủy</p>`;
                 }
             },
             getDateString: function (lockDateTime) {
@@ -61,6 +83,26 @@ app.engine(
                     ":" +
                     ("0" + lockDateTime.getUTCSeconds()).slice(-2);
                 return dateTimeString;
+            },
+            getTransactionTypeString: function (type) {
+                switch (parseInt(type)) {
+                    case 0:
+                        return "Nạp tiền";
+                    case 1:
+                        return "Rút tiền";
+                    case 2:
+                        return "Chuyển tiền";
+                    case 3:
+                        return "Nhận tiền";
+                    case 4:
+                        return "Mua thẻ điện thoại";
+                }
+            },
+            getPriceString: function (price) {
+                return parseInt(price).toLocaleString("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                });
             },
         },
     })
@@ -95,5 +137,5 @@ route(app);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`>>> App listening on port ${PORT}.`);
+    console.log(`>>> App listening on port ${PORT}`);
 });
